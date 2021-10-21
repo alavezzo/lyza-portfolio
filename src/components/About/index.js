3824851d749f1dffc0365ad3be1eb02554cf6645
bowdenshot <anthonyelavezzo@gmail.com>
2021-10-21 12:14:04 -0700

    about section renders and link is active upon click

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import coverImage from '../../assets/cover/cover-image.jpg';
 
+
+
 const About = () => {
+
     return (
         <section className="my-5">
             <h1 id="about">Who am I?</h1>
